53096168dea2776cf85bf9344a0f0c3bf5412b8b
ChrisLo0751 <73991742+ChrisLo0751@users.noreply.github.com>
2022-07-09 22:22:24 +0800

    style

--- main.go
+++ main.go
@@ -74,6 +74,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "CodeRepo")
 		os.Exit(1)
 	}
+	if err = (&controllers.ClusterReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Cluster"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Cluster")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
